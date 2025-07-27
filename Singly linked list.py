891080f4db299632bee8e9b14119c177cb5e5b72
Vaishnavi <vaishnavi14102006@gmail.com>
2025-07-27 18:48:55 +0530

    Add files via upload

---
+++ Singly linked list.py
@@ -0,0 +1,67 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+class LinkedList:
+    def __init__(self):
+        self.head=None
+    def is_empty(self):
+        return self.head is None
+    def append(self,data):
+        new_node=Node(data)
+        if self.head is None:
+            self.head=new_node
+            return
+        current=self.head
+        while current.next:
+            current=current.next
+        current.next=new_node
+    def prepend(self,data):
+        new_node=Node(data)
+        new_node.next=self.head
+        self.head=new_node
+    def delete(self,data):
+        if self.head is None:
+            return
+        if self.head.data==data:
+            self.head=self.head.next
+            return
+        current=self.head
+        while current.next:
+            if current.next.data==data:
+                current.next=current.next.next
+                return
+            current=current.next
+    def search(self,data):
+        current=self.head
+        while current:
+            if current.data==data:
+                return True
+            current=current.next
+        return False
+    def display(self):
+        current=self.head
+        while current:
+            print(current.data,end="->")
+            current=current.next
+        print("None")
+linked_list=LinkedList()
+linked_list.append(1)
+linked_list.append(2)
+linked_list.append(3)
+print("Linked list:")
+linked_list.display()
+linked_list.prepend(0)
+linked_list.prepend(-1)
+print("Linked list after prepend:")
+linked_list.display()
+linked_list.delete(0)
+linked_list.delete(3)
+print("Linked list after deletion:")
+linked_list.display()
+print("Search for 1 :",linked_list.search(1))
+print("Search for 5 :",linked_list.search(5))
+
+
+
+    
